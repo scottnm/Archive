288105f47cc74a908006277adbb8d7b7756149d0
Scott Munro <scottmunro@utexas.edu>
2015-09-26 17:53:00 -0500

    generating sound using mml

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 $('#submit-btn').click(function(){
 	var inputText = $('#text-input').val();
-	alert(inputText);
+	generate_sound(inputText);
+	//alert(inputText);
 });
+
+function generate_sound(input) {
+	var gen = T("PluckGen", {env:{type:"perc", r:500, lv:0.4}}).play();
+	T("mml", {mml:input}, gen).start();
+}
