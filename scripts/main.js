9b2121b841b19d082dea9c859d35ef33317033a4
Scott Munro <scottmunro@utexas.edu>
2015-09-26 20:10:13 -0500

    Basic mml parsing on word structures Resolves #2 Resolves #3

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,108 @@
 $('#submit-btn').click(function(){
 	var inputText = $('#text-input').val();
-	if(inputText == "") {
+	if(inputText == '') {
 		return;
 	}
-	//split input into array
-	var input = inputText.split(" ");
-	//process duration of each element in array
-	duration_preprocess(input);
-	//join array into string
-	var mmlText = input.join(" ");
-	//pass to generate_sound
-	generate_sound(mmlText);
+
+	var textSplit = inputText.split(' ');
+	var mmlSplit = text_to_mml(textSplit);
+	var mmlCmd = generate_mml_command(mmlSplit);
+	generate_sound(mmlCmd);
 	
-	$('#text-input').val("");
-	//alert(inputText);
+	$('#text-input').val('');
+	console.log(mmlCmd);
 });
 
-function duration_preprocess(textChunks){
-	for (var i in textChunks) {
-		switch(textChunks[i].length){
-			case 0:
-				break;
-			case 1:
-				textChunks[i] = "l16 " + textChunks[i];
-				break;
-			case 2:
-				textChunks[i] = "l12 " + textChunks[i];
-				break;
-			case 3:
-				textChunks[i] = "l8 " + textChunks[i];
-				break;	
-			case 4:
-				textChunks[i] = "l6 " + textChunks[i];
-				break;	
-			case 5:
-				textChunks[i] = "l4 " + textChunks[i];
-				break;	
-			case 6:
-			case 7:
-				textChunks[i] = "l3 " + textChunks[i];
-				break;
-			case 8:
-			case 9:
-				textChunks[i] = "l2 " + textChunks[i];
-				break;		
-			case 10:
-			case 11:
-				textChunks[i] = "l1 " + textChunks[i];
-				break;
-			default:
-				textChunks[i] = "l1 " + textChunks[i] + "&" + textChunks[i];
-				break;		
+function generate_mml_command(mmlSplit) {
+	var mmlCmd = '';
+	mmlSplit.forEach(function(note) {
+		var text = note.length + ' ' + note.octave + ' ' + note.note;
+		if (note.modifiers.flat) {
+			text += '-';
+		} else if (note.modifiers.sharp) {
+			text += '+';
 		}
+		
+		if (note.modifiers.dot) {
+			text += '.';
+		}
+		
+		if (note.modifiers.pitchbend && !note.modifiers.repetition) {
+			text += '*';
+		} else if (note.modifiers.repetition) {
+			text = '[ ' + text + ' ]';
+		}
+		
+		if (!note.modifiers.pitchbend) {
+			text += ' ';
+		}
+		
+		mmlCmd += text;
+	});
+	return mmlCmd;
+}
+
+function MmlAttribute(initText) {
+	this.initText = initText;
+	this.length = get_duration(initText);
+	this.note = get_note(initText);
+	this.octave = get_octave(initText);
+	this.modifiers = new Modifiers(initText);
+}
+
+
+function text_to_mml(textArr) {
+	var mmlArr = [];
+	textArr.forEach(function(text){
+		mmlArr.push(new MmlAttribute(text));
+	});
+	
+	return mmlArr;
+}
+ 
+function get_note(text) {
+	return 'a';
+}
+
+function get_octave(text) {
+	return 'o4';
+}
+
+function Modifiers(text) {
+	this.flat =       text.indexOf('x') != -1 && text.indexOf('j') == -1;
+	this.sharp =      text.indexOf('j') != -1 && text.indexOf('x') == -1;
+	this.dot =        text.indexOf('k') != -1;
+	this.repetition = text.indexOf('q') != -1;
+	this.pitchbend =  text.indexOf('z') != -1;
+}
+
+function get_duration(text) {
+	switch(text.length) {
+		case 0:
+			break;
+		case 1:
+			return 'l16'
+		case 2:
+			return 'l12';
+		case 3:
+			return 'l8';
+		case 4:
+			return 'l6';
+		case 5:
+			return 'l4';
+		case 6:
+		case 7:
+			return 'l3';
+		case 8:
+		case 9:
+			return 'l2';
+		default:
+			return 'l1';
 	}
 }
 
 function generate_sound(input) {
-	var gen = T("PluckGen", {env:{type:"perc", r:500, lv:0.4}}).play();
-	T("mml", {mml:input}, gen).start();
+	var gen = T('PluckGen', {env:{type:'perc', r:500, lv:0.4}}).play();
+	T('mml', {mml:input}, gen).start();
 }
 
