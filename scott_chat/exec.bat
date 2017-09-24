fe77461c6b5929020183f75e07edc6a19b6cc388
Scott Munro <scottnmunro@gmail.com>
2017-09-24 10:50:30 -0700

    test command is now my exec script and auto closes windows when program is done

--- scott_chat/exec.bat
+++ scott_chat/exec.bat
@@ -1,1 +1,2 @@
-.\scott_chat.exe
+start %windir%\system32\cmd.exe /S /C "cd %cd% && .\scott_chat.exe server"
+start %windir%\system32\cmd.exe /S /C "cd %cd% && .\scott_chat.exe client"
--- scott_chat/test.cmd
+++
@@ -1,2 +0,0 @@
-start %windir%\system32\cmd.exe /S /K "cd %cd% && .\scott_chat.exe server"
-start %windir%\system32\cmd.exe /S /K "cd %cd% && .\scott_chat.exe client"
