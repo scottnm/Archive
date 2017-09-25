285b6fa188fe3846164a17417c8380b2f048f8d3
Scott Munro <scottnmunro@gmail.com>
2017-09-24 19:29:03 -0700

    changed ifndef guards to pragma once for code cleanliness/consistency

--- scott_chat/client.h
+++ scott_chat/client.h
@@ -1,9 +1,6 @@
-#ifndef __CLIENT_H__
-#define __CLIENT_H__
+#pragma once
 
 namespace client
 {
     SOCKET create_socket(const char* hostname);
 }
-
-#endif //__CLIENT_H__
--- scott_chat/server.h
+++ scott_chat/server.h
@@ -1,9 +1,6 @@
-#ifndef __SERVER_H__
-#define __SERVER_H__
+#pragma once
 
 namespace server
 {
     SOCKET create_socket(const char* hostname);
 }
-
-#endif //__SERVER_H__
