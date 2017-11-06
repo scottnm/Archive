eb7858c8e01dacd217347a7be8c4710de75a0df3
Scott Munro <scottnmunro@gmail.com>
2017-11-05 17:03:03 -0800

    closes #2. resizing handled

--- tui.cpp
+++ tui.cpp
@@ -14,14 +14,17 @@ namespace tui
     static void draw_proc();
     static void reserve_buffers();
     static void update_divider(uint32_t line_width);
-    static void update_screen_buffers();
+    static void create_screen_buffers();
+    static void update_screen_buffer_info_if_necessary();
     static void clear_screen();
+    static uint16_t input_window_height();
+    static uint16_t conversation_window_height();
 
     static std::mutex screen_buffer_mutex;
 
     static HANDLE display_console;
     static HANDLE write_buffer_console;
-    static CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    static CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info = {0};
 
     static std::vector<char> conversation_buffer;
     static uint32_t conversation_offset = 0;
@@ -29,30 +32,19 @@ namespace tui
     static std::vector<char> input_buffer;
     static uint32_t input_offset = 0;
 
-    static uint16_t conversation_window_height;
-    static uint16_t input_window_height;
-
     static const char* divider = nullptr;
 
     std::thread init()
     {
-        update_screen_buffers();
+        create_screen_buffers();
+        reserve_buffers();
         update_divider(screen_buffer_info.dwSize.X);
         SetConsoleActiveScreenBuffer(display_console);
-
-        auto screen_height = screen_buffer_info.dwSize.Y;
-        // 1 line for the scroll buffer, 1 line for the divider, 2 line for the input field
-        input_window_height = 2;
-        conversation_window_height = screen_height - 2 - input_window_height;
-
-        reserve_buffers();
-
         return std::thread(draw_proc);
     }
 
-    void update_screen_buffers()
+    void create_screen_buffers()
     {
-        // TODO: support resizing by freeing old screen buffers
         display_console = CreateConsoleScreenBuffer(
                 GENERIC_WRITE | GENERIC_READ,
                 FILE_SHARE_WRITE | FILE_SHARE_READ,
@@ -61,11 +53,6 @@ namespace tui
                 nullptr);
         assert (display_console != INVALID_HANDLE_VALUE);
 
-        auto got_buffer_info = GetConsoleScreenBufferInfo(
-                display_console,
-                &screen_buffer_info);
-        assert (got_buffer_info);
-
         write_buffer_console = CreateConsoleScreenBuffer(
                 GENERIC_WRITE | GENERIC_READ,
                 FILE_SHARE_WRITE | FILE_SHARE_READ,
@@ -73,9 +60,36 @@ namespace tui
                 CONSOLE_TEXTMODE_BUFFER,
                 nullptr);
         assert (write_buffer_console != INVALID_HANDLE_VALUE);
+    }
 
-        auto set_console_info = SetConsoleWindowInfo(write_buffer_console, true, &screen_buffer_info.srWindow);
-        assert(set_console_info);
+    static bool rect_eq(SMALL_RECT* w1, SMALL_RECT* w2)
+    {
+        auto wp1 = reinterpret_cast<uint64_t*>(w1);
+        auto wp2 = reinterpret_cast<uint64_t*>(w2);
+        return *wp1 == *wp2;
+    }
+
+    void update_screen_buffer_info_if_necessary()
+    {
+        CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+        auto got_buffer_info = GetConsoleScreenBufferInfo(
+                display_console,
+                &buffer_info);
+        assert (got_buffer_info);
+
+        if (! rect_eq(&buffer_info.srWindow, &screen_buffer_info.srWindow))
+        {
+            screen_buffer_info = buffer_info;
+            CloseHandle(write_buffer_console);
+            write_buffer_console = CreateConsoleScreenBuffer(
+                    GENERIC_WRITE | GENERIC_READ,
+                    FILE_SHARE_WRITE | FILE_SHARE_READ,
+                    nullptr,
+                    CONSOLE_TEXTMODE_BUFFER,
+                    nullptr);
+            assert (write_buffer_console != INVALID_HANDLE_VALUE);
+            update_divider(screen_buffer_info.dwSize.X);
+        }
     }
 
     void push_to_input_field(char c)
@@ -111,11 +125,12 @@ namespace tui
         std::unique_lock<std::mutex> screen_buffer_guard(screen_buffer_mutex, std::defer_lock);
         while (globals::application_running)
         {
+            update_screen_buffer_info_if_necessary();
             screen_buffer_guard.lock();
 
             auto cursor_pos = c_console_origin;
             auto screen_width = screen_buffer_info.dwSize.X;
-            uint32_t conversation_chars_left = conversation_window_height * screen_width;
+            uint32_t conversation_chars_left = conversation_window_height() * screen_width;
             char* msg_ptr = conversation_buffer.data();
             char* msgs_end = msg_ptr + conversation_buffer.size();
             while (msg_ptr < msgs_end && conversation_chars_left > 0)
@@ -132,7 +147,7 @@ namespace tui
                 msg_ptr += next_string_size + 1;
                 cursor_pos.Y += lines_to_write;
             }
-            cursor_pos.Y = conversation_window_height;
+            cursor_pos.Y = conversation_window_height();
             SetConsoleCursorPosition(write_buffer_console, cursor_pos);
             WriteFile(write_buffer_console, divider, screen_buffer_info.dwSize.X, nullptr, nullptr);
             WriteFile(write_buffer_console, input_buffer.data(), input_buffer.size(), nullptr, nullptr);
@@ -166,14 +181,26 @@ namespace tui
     void reserve_buffers()
     {
         auto screen_width = screen_buffer_info.dwSize.X;
-        conversation_buffer.reserve(conversation_window_height * screen_width);
-        input_buffer.reserve(input_window_height * screen_width);
+        conversation_buffer.reserve(conversation_window_height() * screen_width);
+        input_buffer.reserve(input_window_height() * screen_width);
     }
 
-	void clear_screen()
-	{
+    void clear_screen()
+    {
         auto screen_size = screen_buffer_info.dwSize.X * screen_buffer_info.dwSize.Y;
         DWORD chars_read;
         FillConsoleOutputCharacter(write_buffer_console, ' ', screen_size, c_console_origin, &chars_read);
-	}
+    }
+
+    uint16_t input_window_height()
+    {
+        return 2;
+    }
+
+    uint16_t conversation_window_height()
+    {
+        // 1 line for the scroll buffer, 1 line for the divider, 2 line for the input field
+        return screen_buffer_info.dwSize.Y - 2 - input_window_height();
+    }
+
 }
