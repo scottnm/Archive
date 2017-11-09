eb049eda1a5bd7852500471c41459447e40c6d11
Scott Munro <scottnmunro@gmail.com>
2017-11-08 19:20:42 -0800

    getting a temp filename for the log

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
+#include <cstdio>
+#include <cstdlib>
 #include <windows.h>
 #pragma comment (lib, "kernel32.lib")
 
-int main()
+static
+const char*
+get_temp_filepath()
+{
+    static auto temp_filename = "\\time_pest_temp.txt";
+    static auto temp_filename_len = strlen(temp_filename);
+    auto temp_dir = getenv("TEMP");
+    auto temp_dir_len = strlen(temp_dir);
+
+    char* path = new char[temp_dir_len + temp_filename_len + 1];
+    memcpy_s(path, temp_dir_len + temp_filename_len, temp_dir, temp_dir_len);
+    memcpy_s(path + temp_dir_len, temp_filename_len, temp_filename, temp_filename_len);
+    path[temp_dir_len + temp_filename_len] = '\0';
+
+    return path;
+}
+
+int
+main()
 {
     // open up notepad.exe with a tmp file name
     STARTUPINFO startup_info = {0};
@@ -20,6 +40,9 @@ int main()
 
     auto ret = WaitForSingleObject(proc_info.hProcess, INFINITE /* swap out for timeout? */);
     (void)ret;
+
+    auto temp_path = get_temp_filepath();
+    printf("%s\n", temp_path);
 /*
 BOOL WINAPI CreateProcess(
   _In_opt_    LPCTSTR               lpApplicationName,
@@ -41,6 +64,7 @@ BOOL WINAPI CreateProcess(
     // free resources
     CloseHandle(proc_info.hProcess);
     CloseHandle(proc_info.hThread);
+    delete[] temp_path;
 
     // quit
     return 0;
