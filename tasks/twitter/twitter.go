152027793aa8f5609586dba78afd37f25b2c3898
Scott Munro <scottnmunro@gmail.com>
2018-01-14 17:02:21 -0800

    using the twitter client to authorize use as a user

--- tasks/twitter/twitter.go
+++ tasks/twitter/twitter.go
@@ -6,10 +6,14 @@ import (
     "github.com/kurrik/oauth1a"
     "github.com/kurrik/twittergo"
     "io/ioutil"
+    "net/http"
+    "strconv"
 )
 
 type TwitterFeeder struct {
     ScreenName string
+    Id uint64
+    Name string
 }
 
 func GetStringJsonFromFile(fileName string) map[string]string {
@@ -41,13 +45,31 @@ func NewTwitterFeeder(feedCredentials, feedConfig string) *TwitterFeeder {
     credentials := GetStringJsonFromFile(feedCredentials)
     twitterClient := GetClient(credentials["ConsumerKey"], credentials["ConsumerSecret"], credentials["AccessToken"], credentials["AccessTokenSecret"])
 
-    fmt.Printf("Credentials...\n")
-    fmt.Printf("\tCredentialsObj: %s\n", credentials)
-    fmt.Printf("\tTwitterClient: %s\n", twitterClient)
-    fmt.Printf("...End Credentials\n")
+    req, err := http.NewRequest("GET", "/1.1/account/verify_credentials.json", nil)
+    if err != nil {
+        panic(fmt.Sprintf("Could not parse request: %v\n", err))
+    }
+
+    resp, err := twitterClient.SendRequest(req)
+    if err != nil {
+        panic(fmt.Sprintf("Could not send request: %v\n", err))
+    }
+
+    user := &twittergo.User{}
+    err = resp.Parse(user)
+    if err != nil {
+        panic(fmt.Sprintf("Could not parse response: %v\n", err))
+    }
+
+    fmt.Printf("ID:\t%v\n", user.Id())
+    fmt.Printf("Name:\t%v\n", user.Name())
+
+    tf.Id = user.Id()
+    tf.Name = user.Name()
+
     return tf
 }
 
 func (tf *TwitterFeeder) GetFeed () []string {
-    return []string{tf.ScreenName}
+    return []string{tf.ScreenName, strconv.FormatUint(tf.Id, 10), tf.Name}
 }
