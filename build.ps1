019e26aae597577f52566eaf40aaa909d33b3a61
Scott Munro <scottnmunro@gmail.com>
2022-02-18 18:24:08 -0800

    add a nuget restore option

--- build.ps1
+++ build.ps1
@@ -1,9 +1,11 @@
 Param(
     [ValidateSet("Debug", "Release")]
     [string]$Configuration="Debug",
-
+    [switch]$NugetRestore,
     [switch]$Clean
     )
 
+$restoreArg = if ($NugetRestore) { "-t:restore" } else {}
 $targetArg = if ($Clean) { "-t:Clean" } else { "" }
-MSBuild journal.sln -verbosity:normal -p:Configuration=$Configuration $targetArg
+
+MSBuild -m journal.sln -verbosity:normal -p:Configuration=$Configuration $targetArg $restoreArg
